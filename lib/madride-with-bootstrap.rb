5d99e496dde3b056abb0353e21f2acca4df47e08
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-27 19:28:03 +0200

    Fix paths

--- lib/madride-with-bootstrap.rb
+++ lib/madride-with-bootstrap.rb
@@ -1,2 +1,2 @@
-Madride.append_path File.expand_path('../stylesheets', __FILE__)
-Madride.append_path File.expand_path('../javascripts', __FILE__)
+Madride.append_path File.expand_path('../../stylesheets', __FILE__)
+Madride.append_path File.expand_path('../../javascripts', __FILE__)
